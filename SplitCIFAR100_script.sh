45521397cc31ae39654f64400893030c67abc48f
lilanpei <lilanpei@gmail.com>
2024-01-15 18:15:52 +0100

    Update scripts

--- SplitCIFAR100_script.sh
+++ SplitCIFAR100_script.sh
@@ -1,11 +1,11 @@
-python main.py -sn "JointTraining" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 --logdir "./logs/SplitCIFAR100_logs/run1_epochs_joint_200_CL_20" 
-python main.py -sn "JointTraining" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 --logdir "./logs/SplitCIFAR100_logs/run1_epochs_joint_200_CL_20" -stcm
-python main.py -sn "JointTraining" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 --logdir "./logs/SplitCIFAR100_logs/run1_epochs_joint_200_CL_20" -ppcm
-python main.py -sn "Naive" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 20 -lr 0.1 --logdir "./logs/SplitCIFAR100_logs/run1_epochs_joint_200_CL_20"
-python main.py -sn "Naive" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 20 -lr 0.1 --logdir "./logs/SplitCIFAR100_logs/run1_epochs_joint_200_CL_20" -stcm
-python main.py -sn "Naive" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 20 -lr 0.1 --logdir "./logs/SplitCIFAR100_logs/run1_epochs_joint_200_CL_20" -ppcm
-python main.py -sn "Naive" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 20 -lr 0.1 --logdir "./logs/SplitCIFAR100_logs/run1_epochs_joint_200_CL_20" -ppcm -ppdm
-python main.py -sn "Replay" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 20 -lr 0.1 --logdir "./logs/SplitCIFAR100_logs/run1_epochs_joint_200_CL_20"
-python main.py -sn "Replay" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 20 -lr 0.1 --logdir "./logs/SplitCIFAR100_logs/run1_epochs_joint_200_CL_20" -stcm
-python main.py -sn "Replay" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 20 -lr 0.1 --logdir "./logs/SplitCIFAR100_logs/run1_epochs_joint_200_CL_20" -ppcm
-python main.py -sn "Replay" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 20 -lr 0.1 --logdir "./logs/SplitCIFAR100_logs/run1_epochs_joint_200_CL_20" -ppcm -ppdm
+python main.py -sn "JointTraining" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_3" 
+python main.py -sn "JointTraining" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_3" -stcm
+python main.py -sn "JointTraining" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_3" -ppcm
+python main.py -sn "Naive" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_3"
+python main.py -sn "Naive" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_3" -stcm
+python main.py -sn "Naive" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_3" -ppcm
+python main.py -sn "Naive" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_3" -ppcm -ppdm
+python main.py -sn "Replay" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_3"
+python main.py -sn "Replay" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_3" -stcm
+python main.py -sn "Replay" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_3" -ppcm
+python main.py -sn "Replay" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_3" -ppcm -ppdm
--- SplitMNIST_script.sh
+++ SplitMNIST_script.sh
@@ -1,11 +1,11 @@
-python main.py -sn "JointTraining" -tp 50 --logdir "./logs/SplitMNIST_logs/run3_epochs_joint_50_CL_20_patience_3" 
-python main.py -sn "JointTraining" -tp 50 --logdir "./logs/SplitMNIST_logs/run3_epochs_joint_50_CL_20_patience_3" -stcm
-python main.py -sn "JointTraining" -tp 50 --logdir "./logs/SplitMNIST_logs/run3_epochs_joint_50_CL_20_patience_3" -ppcm
-python main.py -sn "Naive" -tp 20 --logdir "./logs/SplitMNIST_logs/run3_epochs_joint_50_CL_20_patience_3"
-python main.py -sn "Naive" -tp 20 --logdir "./logs/SplitMNIST_logs/run3_epochs_joint_50_CL_20_patience_3" -stcm
-python main.py -sn "Naive" -tp 20 --logdir "./logs/SplitMNIST_logs/run3_epochs_joint_50_CL_20_patience_3" -ppcm
-python main.py -sn "Naive" -tp 20 --logdir "./logs/SplitMNIST_logs/run3_epochs_joint_50_CL_20_patience_3" -ppcm -ppdm
-python main.py -sn "Replay" -tp 20 --logdir "./logs/SplitMNIST_logs/run3_epochs_joint_50_CL_20_patience_3"
-python main.py -sn "Replay" -tp 20 --logdir "./logs/SplitMNIST_logs/run3_epochs_joint_50_CL_20_patience_3" -stcm
-python main.py -sn "Replay" -tp 20 --logdir "./logs/SplitMNIST_logs/run3_epochs_joint_50_CL_20_patience_3" -ppcm
-python main.py -sn "Replay" -tp 20 --logdir "./logs/SplitMNIST_logs/run3_epochs_joint_50_CL_20_patience_3" -ppcm -ppdm
+python main.py -sn "JointTraining" -tp 20 -ep --logdir "./logs/SplitMNIST_logs/run4_epochs_20_ep_3" 
+python main.py -sn "JointTraining" -tp 20 -ep --logdir "./logs/SplitMNIST_logs/run4_epochs_20_ep_3" -stcm
+python main.py -sn "JointTraining" -tp 20 -ep --logdir "./logs/SplitMNIST_logs/run4_epochs_20_ep_3" -ppcm
+python main.py -sn "Naive" -tp 20 -ep --logdir "./logs/SplitMNIST_logs/run4_epochs_20_ep_3"
+python main.py -sn "Naive" -tp 20 -ep --logdir "./logs/SplitMNIST_logs/run4_epochs_20_ep_3" -stcm
+python main.py -sn "Naive" -tp 20 -ep --logdir "./logs/SplitMNIST_logs/run4_epochs_20_ep_3" -ppcm
+python main.py -sn "Naive" -tp 20 -ep --logdir "./logs/SplitMNIST_logs/run4_epochs_20_ep_3" -ppcm -ppdm
+python main.py -sn "Replay" -tp 20 -ep --logdir "./logs/SplitMNIST_logs/run4_epochs_20_ep_3"
+python main.py -sn "Replay" -tp 20 -ep --logdir "./logs/SplitMNIST_logs/run4_epochs_20_ep_3" -stcm
+python main.py -sn "Replay" -tp 20 -ep --logdir "./logs/SplitMNIST_logs/run4_epochs_20_ep_3" -ppcm
+python main.py -sn "Replay" -tp 20 -ep --logdir "./logs/SplitMNIST_logs/run4_epochs_20_ep_3" -ppcm -ppdm
