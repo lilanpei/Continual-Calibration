ad17870a3fe7abdb992b0bcb2a7d67af572bc4fd
lilanpei <lilanpei@gmail.com>
2024-01-16 08:36:28 +0100

    Update the scripts and plotting

--- SplitCIFAR100_script.sh
+++ SplitCIFAR100_script.sh
@@ -5,7 +5,7 @@ python main.py -sn "Naive" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -
 python main.py -sn "Naive" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep -p 10 --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_10" -stcm
 python main.py -sn "Naive" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep -p 10 --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_10" -ppcm
 python main.py -sn "Naive" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep -p 10 --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_10" -ppcm -ppdm
-python main.py -sn "Replay" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep -p 10 --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_10"
-python main.py -sn "Replay" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep -p 10 --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_10" -stcm
-python main.py -sn "Replay" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep -p 10 --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_10" -ppcm
-python main.py -sn "Replay" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ep -p 10 --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_10" -ppcm -ppdm
+python main.py -sn "Replay" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ms 4000 -ep -p 10 --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_10"
+python main.py -sn "Replay" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ms 4000 -ep -p 10 --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_10" -stcm
+python main.py -sn "Replay" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ms 4000 -ep -p 10 --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_10" -ppcm
+python main.py -sn "Replay" -dn "SplitCIFAR100" -ts 128 -es 128 -tp 200 -lr 0.1 -ms 4000 -ep -p 10 --logdir "./logs/SplitCIFAR100_logs/run2_epochs_200_ep_10" -ppcm -ppdm
