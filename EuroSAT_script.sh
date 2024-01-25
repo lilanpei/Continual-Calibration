f185c629071d9b165fb700a9e16b9482eb099048
lilanpei <lilanpei@gmail.com>
2024-01-26 00:30:16 +0100

    Revise scripts and update plotting

--- EuroSAT_script.sh
+++ EuroSAT_script.sh
@@ -1,21 +1,21 @@
-python main.py -sn "JointTraining" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 --logdir "./logs/EuroSAT_logs/run4_epochs_100" 
-python main.py -sn "JointTraining" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 --logdir "./logs/EuroSAT_logs/run4_epochs_100" -stcm
-python main.py -sn "JointTraining" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 --logdir "./logs/EuroSAT_logs/run4_epochs_100" -ppcm
-python main.py -sn "JointTraining" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 --logdir "./logs/EuroSAT_logs/run4_epochs_100" -ppcm -ppvs
-python main.py -sn "JointTraining" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 --logdir "./logs/EuroSAT_logs/run4_epochs_100" -ppcm -ppms
-python main.py -sn "Naive" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 -ep --logdir "./logs/EuroSAT_logs/run4_epochs_100"
-python main.py -sn "Naive" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 -ep --logdir "./logs/EuroSAT_logs/run4_epochs_100" -stcm
-python main.py -sn "Naive" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 -ep --logdir "./logs/EuroSAT_logs/run4_epochs_100" -ppcm
-python main.py -sn "Naive" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 -ep --logdir "./logs/EuroSAT_logs/run4_epochs_100" -ppcm -ppdm
-python main.py -sn "Naive" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 -ep --logdir "./logs/EuroSAT_logs/run4_epochs_100" -ppcm -ppvs
-python main.py -sn "Naive" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 -ep --logdir "./logs/EuroSAT_logs/run4_epochs_100" -ppcm -ppvs -ppdm
-python main.py -sn "Naive" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 -ep --logdir "./logs/EuroSAT_logs/run4_epochs_100" -ppcm -ppms
-python main.py -sn "Naive" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 -ep --logdir "./logs/EuroSAT_logs/run3_epochs_100" -ppcm -ppms -ppdm
-python main.py -sn "Replay" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 -ms 2000 --logdir "./logs/EuroSAT_logs/run4_epochs_100"
-python main.py -sn "Replay" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 -ms 2000 --logdir "./logs/EuroSAT_logs/run4_epochs_100" -stcm
-python main.py -sn "Replay" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 -ms 2000 --logdir "./logs/EuroSAT_logs/run4_epochs_100" -ppcm
-python main.py -sn "Replay" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 -ms 2000 --logdir "./logs/EuroSAT_logs/run4_epochs_100" -ppcm -ppdm
-python main.py -sn "Replay" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 -ms 2000 --logdir "./logs/EuroSAT_logs/run4_epochs_100" -ppcm -ppvs
-python main.py -sn "Replay" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 -ms 2000 --logdir "./logs/EuroSAT_logs/run4_epochs_100" -ppcm -ppvs -ppdm
-python main.py -sn "Replay" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 -ms 2000 --logdir "./logs/EuroSAT_logs/run4_epochs_100" -ppcm -ppms
-python main.py -sn "Replay" -dn "EuroSAT" -cid 0 -ts 128 -es 128 -tp 100 -lr 0.01 -ms 2000 --logdir "./logs/EuroSAT_logs/run4_epochs_100" -ppcm -ppms -ppdm
+python main.py -sn "JointTraining" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" 
+python main.py -sn "JointTraining" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -stcm
+python main.py -sn "JointTraining" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -ppcm
+python main.py -sn "JointTraining" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -ppcm -ppvs
+python main.py -sn "JointTraining" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -ppcm -ppms
+python main.py -sn "Naive" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100"
+python main.py -sn "Naive" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -stcm
+python main.py -sn "Naive" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -ppcm
+python main.py -sn "Naive" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -ppcm -ppdm
+python main.py -sn "Naive" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -ppcm -ppvs
+python main.py -sn "Naive" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -ppcm -ppvs -ppdm
+python main.py -sn "Naive" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -ppcm -ppms
+python main.py -sn "Naive" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -ppcm -ppms -ppdm
+python main.py -sn "Replay" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ms 2000 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100"
+python main.py -sn "Replay" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ms 2000 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -stcm
+python main.py -sn "Replay" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ms 2000 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -ppcm
+python main.py -sn "Replay" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ms 2000 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -ppcm -ppdm
+python main.py -sn "Replay" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ms 2000 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -ppcm -ppvs
+python main.py -sn "Replay" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ms 2000 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -ppcm -ppvs -ppdm
+python main.py -sn "Replay" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ms 2000 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -ppcm -ppms
+python main.py -sn "Replay" -dn "EuroSAT" -cid 1 -ts 128 -es 128 -tp 100 -lr 0.01 -ms 2000 -ep -p 10 --logdir "./logs/EuroSAT_logs/run7_epochs_100" -ppcm -ppms -ppdm
